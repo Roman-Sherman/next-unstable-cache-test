3c5f616244163c1b472eb678d03fec73114f7794
Roman-Sherman <firedev43@gmail.com>
2024-10-27 20:08:28 -0700

    init

---
+++ test/src/app/page.tsx
@@ -0,0 +1,29 @@
+import { unstable_cache } from "next/cache";
+import Image from "next/image";
+import { cache } from "react";
+export const dynamic = "force-dynamic";
+let fetchCount = 0;
+const fetchData = unstable_cache(
+  async () => {
+    fetchCount++;
+    return "hello";
+  },
+  ["data"],
+  {
+    revalidate: 500,
+  }
+);
+
+export default async function Home() {
+  const p1 = fetchData();
+  const p2 = fetchData();
+  const p3 = fetchData();
+  await Promise.all([p1, p2, p3]);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        {fetchCount}
+      </main>
+    </div>
+  );
+}
